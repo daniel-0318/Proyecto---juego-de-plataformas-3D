2b473ad3a4a9da650c7b5e544da82459230b28c4
daniel0318 <dani_0318@hotmail.com>
2017-05-10 22:40:39 -0500

    Comenzando con la seleccion del personaje

---
+++ Assets/Scripts/SeleccionarPersonaje.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeleccionarPersonaje : MonoBehaviour {
+
+	private GameObject[] lista_personajes;
+
+	// Use this for initialization
+	void Start () {
+		lista_personajes = new GameObject[transform.childCount];
+
+		//Se llena la lista con los modelos (personajes)
+		for (int i; i < transform.childCount; i++) {
+			lista_personajes [i] = transform.GetChild (i).gameObject;
+		}
+
+		foreach(GameObject go in lista_personajes){
+			go.SetActive (false);
+		}
+	}
+
+}
