8eccf07bcb92188974ba0098efe675de3d39b8fe
daniel0318 <dani_0318@hotmail.com>
2017-05-10 23:26:03 -0500

    Botones
    Los botones de atras y adeltante ya funcionan

--- Assets/Scripts/SeleccionarPersonaje.cs
+++ Assets/Scripts/SeleccionarPersonaje.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class SeleccionarPersonaje : MonoBehaviour {
 
 	private GameObject[] lista_personajes;
+	private int index = 0;
 
 	// Use this for initialization
-	void Start () {
+	private void Start () {
 		lista_personajes = new GameObject[transform.childCount];
 
 		//Se llena la lista con los modelos (personajes)
@@ -24,6 +25,30 @@ public class SeleccionarPersonaje : MonoBehaviour {
 		if (lista_personajes [0]) {
 			lista_personajes [0].SetActive (true);
 		}
+	}
+
+	public void botonIzq(){
+		//dejamos de mostrar el primer personaje para darle paso al siguiente.
+		lista_personajes[index].SetActive(false);
+		index--;
+		//si estabamos parados en el primer personaje se va al ultimo.
+		if (index < 0) {
+			index = lista_personajes.Length - 1;
+		}
+		//se activa el personaje en la posicion index
+		lista_personajes [index].SetActive (true);
+	}
+
+	public void botonDer(){
+		//dejamos de mostrar el primer personaje para darle paso al siguiente.
+		lista_personajes[index].SetActive(false);
+		index++;
+		//si estabamos parados en el primer personaje se va al ultimo.
+		if (index == lista_personajes.Length) {
+			index = 0;
+		}
+		//se activa el personaje en la posicion index
+		lista_personajes [index].SetActive (true);
 	}
 
 }
