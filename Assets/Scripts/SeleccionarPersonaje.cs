1ce547880f5f8ba338209cbca5a69ba4a1e5a123
daniel0318 <dani_0318@hotmail.com>
2017-05-10 23:00:23 -0500

    Botones y personaje
    Los botones ya quedan a lado y lado del personaje

--- Assets/Scripts/SeleccionarPersonaje.cs
+++ Assets/Scripts/SeleccionarPersonaje.cs
@@ -11,12 +11,18 @@ public class SeleccionarPersonaje : MonoBehaviour {
 		lista_personajes = new GameObject[transform.childCount];
 
 		//Se llena la lista con los modelos (personajes)
-		for (int i; i < transform.childCount; i++) {
+		for (int i=0; i < transform.childCount; i++) {
 			lista_personajes [i] = transform.GetChild (i).gameObject;
 		}
 
+		//desactivamos el render de los modelos (para que no se vean todos)
 		foreach(GameObject go in lista_personajes){
 			go.SetActive (false);
+		}
+
+		//activamos que solo se vea el primer personaje en la lista
+		if (lista_personajes [0]) {
+			lista_personajes [0].SetActive (true);
 		}
 	}
 
