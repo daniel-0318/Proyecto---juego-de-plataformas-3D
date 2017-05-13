827fce842169e01844336476680303b402a1b42c
daniel0318 <dani_0318@hotmail.com>
2017-05-13 14:49:51 -0500

    Algunas mejoras
    Timer e intentando acomodar la camara a pie para poder mover con mouse

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject [] lista_personajes;
+	private int jugador_activo;
+	private Vector3 offset; // posicion
+
+	// Use this for initialization
+	void Start () {
+
+		jugador_activo = PlayerPrefs.GetInt ("Seleccion de personaje");
+		/*
+		lista_personajes = new GameObject[transform.childCount];
+
+		//Se llena la lista con los modelos (personajes)
+		for (int i=0; i < transform.childCount; i++) {
+			lista_personajes [i] = transform.GetChild (i).gameObject;
+		}
+
+		//desactivamos el render de los modelos (para que no se vean todos)
+		foreach(GameObject go in lista_personajes){
+			go.SetActive (false);
+		}
+*/
+
+
+		Debug.Log("Tamaño "+lista_personajes.Length);
+		Debug.Log(""+jugador_activo);
+		if (lista_personajes.Length > 0) {
+			lista_personajes [jugador_activo].SetActive (true);
+			offset = transform.position - lista_personajes [jugador_activo].transform.position;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = lista_personajes[jugador_activo].transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/ControladorTiempo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorTiempo : MonoBehaviour {
+
+	public Text contador;
+	public Text fin;
+	private float tiempo = 10f;
+
+	// Use this for initialization
+	void Start () {
+		contador.text = " " + tiempo;
+		fin.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tiempo -= Time.deltaTime;
+		contador.text = " " + tiempo.ToString("f0");
+
+		if(tiempo <= 0)
+		{
+			contador.text = "0";
+			fin.enabled = true;
+		}
+	}
+}
