eaa0703cbe3853123cc0df555308ad75db9e89be
daniel0318 <dani_0318@hotmail.com>
2017-05-13 23:34:42 -0500

    Seguimiento de la cara a medias
    Ya sigue la camara a otro personaje y se hizo algunas plataformas

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,43 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject [] lista_personajes;
+	public GameObject personajePrueba;
 	private int jugador_activo;
 	private Vector3 offset; // posicion
 
 	// Use this for initialization
 	void Start () {
 
-		jugador_activo = PlayerPrefs.GetInt ("Seleccion de personaje");
-		/*
-		lista_personajes = new GameObject[transform.childCount];
-
-		//Se llena la lista con los modelos (personajes)
-		for (int i=0; i < transform.childCount; i++) {
-			lista_personajes [i] = transform.GetChild (i).gameObject;
-		}
-
-		//desactivamos el render de los modelos (para que no se vean todos)
-		foreach(GameObject go in lista_personajes){
-			go.SetActive (false);
-		}
-*/
-
-
-		Debug.Log("Tamaño "+lista_personajes.Length);
-		Debug.Log(""+jugador_activo);
-		if (lista_personajes.Length > 0) {
-			lista_personajes [jugador_activo].SetActive (true);
-			offset = transform.position - lista_personajes [jugador_activo].transform.position;
-		}
-
+		offset = transform.position - personajePrueba.transform.position;
+		//personajePrueba.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = lista_personajes[jugador_activo].transform.position + offset;
+
+		try{
+			Debug.Log("camera");
+			Debug.Log(transform.position);
+			transform.position = personajePrueba.transform.position + offset;
+		}catch(Exception e){
+
+		}
+
 	}
 }
