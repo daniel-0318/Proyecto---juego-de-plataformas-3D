ceff9c9a123c8a23f551d0c8bce14b36f496128e
daniel0318 <dani_0318@hotmail.com>
2017-06-09 23:02:12 -0500

    Juego base terminado
    Solo faltaria ver si se implementa el tiempo

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,31 +7,31 @@ using System;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject []personajes;
 	public GameObject jugado1;
 	public GameObject jugado2;
 	public GameObject jugado3;
 	private Vector3 offset1; // posicion
 	private Vector3 offset2; // posicion
 	private Vector3 offset3; // posicion
+	private Vector3 camara;
 
 	// Use this for initialization
 	void Start () {
 		//Debug.Log ("Camera Controller ENTRO****");
+
 		try{
-			personajes = new GameObject[3];
-			personajes[0] = GameObject.FindGameObjectWithTag("Player1");
-			//Debug.Log("1");
-			personajes[1] = GameObject.FindGameObjectWithTag("Player2");
-			//Debug.Log("2");
-			personajes[2] = GameObject.FindGameObjectWithTag("Player3");
-			//Debug.Log("3");
-			offset1 = transform.position - personajes[0].transform.position;
-			//Debug.Log("4");
-			offset2 = transform.position - personajes[1].transform.position;
-			//Debug.Log("5");
-			offset3 = transform.position - personajes[2].transform.position;
-		//	Debug.Log("6");
+			jugado1 = GameObject.FindGameObjectWithTag("Player1");
+			Debug.Log("1");
+			jugado2 = GameObject.FindGameObjectWithTag("Player2");
+			Debug.Log("2");
+			jugado3 = GameObject.FindGameObjectWithTag("Player3");
+			Debug.Log("3");
+			offset2 = transform.position - jugado2.transform.position;
+			Debug.Log("4");
+			offset3 = transform.position - jugado3.transform.position;
+			Debug.Log("5");
+			offset1 = transform.position - jugado1.transform.position;
+			Debug.Log("6");
 			//Debug.Log("CameraController: Tamaño lista= " + personajes.Length);
 		}catch(Exception e){
 			Debug.Log (e);
@@ -44,12 +44,23 @@ public class CameraController : MonoBehaviour {
 		try{
 			//Debug.Log("camera");
 			//Debug.Log(transform.position);
-			if(personajes[0].activeSelf){
+			if(jugado1.activeSelf){
+				Debug.Log("1");
+				offset1 = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 2, Vector3.up) * offset1;
 				transform.position = jugado1.transform.position + offset1;
-			}else if(personajes[1].activeSelf){
+				transform.LookAt(jugado1.transform.position);
+
+			}else if(jugado2.activeSelf){
+				Debug.Log("2");
+				offset2 = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 2, Vector3.up ) * offset2;
 				transform.position = jugado2.transform.position + offset2;
-			}else if(personajes[2].activeSelf){
+				transform.LookAt(jugado2.transform.position);
+
+			}else if(jugado3.activeSelf){
+				Debug.Log("3");
+				offset3 = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 2, Vector3.up ) * offset3;
 				transform.position = jugado3.transform.position + offset3;
+				transform.LookAt(jugado3.transform.position);
 			}
 
 		}catch(Exception e){
